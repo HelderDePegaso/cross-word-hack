4b21d1d7b9b1d57f5b302f2cec285d77bdd804a2
HelderDePegaso <115958521+HelderDePegaso@users.noreply.github.com>
2025-04-04 14:17:42 +0100

    Creating the algorithm to make seach based on patterns. Enhance ImportMaskArchive.py logic to find mask file by  a length. Implementing debug.

--- ImportMaskArchive.py
+++ ImportMaskArchive.py
@@ -71,6 +71,31 @@ class ImportMaskArchive:
                 inxZ = int(maskName.split("-")[0])
                 if inxZ <= charLength:
                     yield ImportMaskArchive.importOneByName(ImportMaskArchive.maskFilePath(language, maskName))
+                    
+        
+        except ValueError as e:
+            print(e)
+            return None
+    
+
+    @staticmethod
+    def withTheLength(charLength: int, language):
+        """
+        Import all masks with names of a certain length from the top-level package.
+        """
+        try:
+            ImportMaskArchive.validateCharLength(charLength)
+            
+            pathLang = ImportMaskArchive.pathLang(language)
+            masksNames: list = ImportMaskArchive.enlistMasks(language)
+            print("The list")
+            print(masksNames)
+            for maskName in masksNames:
+                # Get the first character in maskName
+                inxZ = int(maskName.split("-")[0])
+                if inxZ == charLength:
+                    yield ImportMaskArchive.importOneByName(ImportMaskArchive.maskFilePath(language, maskName))
+                    
         
         except ValueError as e:
             print(e)
@@ -89,23 +114,35 @@ class ImportMaskArchive:
             return None
 
     @staticmethod
-    def importOneByLength(charLength: int, language: str):
+    def importByLength(charLength: int, language: str, lastElement: int = None):
         """
         Import the first mask with a name of a certain length from the top-level package.
         """
+        print("s")
         try:
+            print("The language is: ", language)
             ImportMaskArchive.validateCharLength(charLength)
-            return {}
+            
         except ValueError as e:
             print(e)
             return None
         
+        importedMasks = list()
+
         masksNames: list = ImportMaskArchive.enlistMasks(language)
         for maskName in masksNames:
             # Get the first character in maskName
             inxZ = int(maskName.split("-")[0])
+            if lastElement:
+                if maskName.startswith(f"{charLength}-0-{lastElement}"):
+                    importedMasks.append(ImportMaskArchive.importOneByName(ImportMaskArchive.maskFilePath(language, maskName)))
+
             if inxZ == charLength:
-                return ImportMaskArchive.importOneByName(ImportMaskArchive.maskFilePath(language, maskName))
+                importedMasks.append(ImportMaskArchive.importOneByName(ImportMaskArchive.maskFilePath(language, maskName)))
+
+
+        return importedMasks
+
 
     @staticmethod
     def importOneByName(mask_name: str):
@@ -113,9 +150,10 @@ class ImportMaskArchive:
         if not os.path.exists(mask_name):
             raise FileNotFoundError("Mask file not found")
         with open(mask_name, 'r') as f:
-            print("The file is: ", f)
+            #print("The file is: ", f)
             package = json.load(f)
-            
+
+        #print(package)    
         return package
 
 
@@ -124,15 +162,19 @@ class ImportMaskArchive:
 
 
 
-
-#print(ImportMaskArchive.importAllFromTop(5, "pt"))
+#print("qrew")
+#mask = list(ImportMaskArchive.importByLength(3, "pt"))
+#
+#
+#print(len(mask))
 
 #class TestImportMaskArchive(unittest.TestCase):
 #    def test_importAllFromTop(self):
 #        self.assertIsInstance(ImportMaskArchive.importAllFromTop(5, "pt"), list)
 
-    #def test_importOneByLength(self):
-    #    self.assertIsInstance(ImportMaskArchive.importOneByLength(5, "pt"), dict)
+    #def test_importByLength(self):
+    #    self.assertIsInstance(ImportMaskArchive.importByLength(5, "pt"), list)
+
     #def test_importOneByName(self):
     #    mask_name = ImportMaskArchive.maskFilePath("pt", "5-test")
     #    self.assertIsInstance(ImportMaskArchive.importOneByName(mask_name), dict)
@@ -146,5 +188,5 @@ class ImportMaskArchive:
     #    masks = ImportMaskArchive.enlistMasks("pt")
     #    self.assertIsInstance(masks, list)
 
-if __name__ == "__main__":
-    unittest.main()
+#if __name__ == "__main__":
+#    unittest.main()
--- index.py
+++ index.py
@@ -1,4 +1,5 @@
 import argparse 
+import unicodedata
 from ImportMaskArchive import ImportMaskArchive
 from extensionFunction import *
 from extensionObjects import ListAsSet
@@ -25,10 +26,83 @@ def simpleSearch(cu:str):
 
 
 def withPatternSearch(cu:str, cp:str):
-    pass
+    
+    if acceptableCharPattern(cp):
+        charPos = getCharPos(cp)
+        
+        initialChar = findInitialChar(cp)
+        finalChar = findFinalChar(cp)
+        initialMask = None
+        finalMask = None
+
+        if initialChar != "":
+            initialMask = initialMaskValue(len(initialChar))
+        
+        if finalChar != "" :
+            finalMask = finalMaskValue(len(finalChar), len(cp)) 
+
+        
+        if initialMask and  not finalMask:
+            selected =  withInitialOrFinalMaskValueSearch(cpLength = len(cp), initialMaskValue = initialMask, charsToUseLength = len(cu))
+
+            print("selected: ", selected)
+            
+        elif not initialMask and finalMask:
+            withInitialOrFinalMaskValueSearch(cpLength = len(cp), finalMaskValue = finalMask)
+
+        elif initialMask and finalMask:
+            withInitialOrFinalMaskValueSearch(cpLength = len(cp), initialMaskValue = initialMask, finalMaskValue = finalMask)
+
+def withInitialOrFinalMaskValueSearch( cpLength: int ,   initialMaskValue: str = None, finalMaskValue: str = None, charsToUseLength: int = None):
+    
+    if not initialMaskValue and not finalMaskValue: 
+        return None
+
+    filtrated = {"high_priority": ListAsSet(), "medium_priority": ListAsSet(), "low_priority": ListAsSet()}
+    
+    
+    maskSetList = list(ImportMaskArchive.importByLength(cpLength, globalLanguage))
+    
+   
+    
+    for mask in maskSetList:
+        
+        if initialMaskValue and not finalMaskValue:
+            filtratedByStartOrEndMaskValue(initialMaskValue, None, mask , filtrated) 
+        
+        elif not initialMaskValue and finalMaskValue:
+            filtratedByStartOrEndMaskValue(None, finalMaskValue, mask , filtrated) 
+
+        elif initialMaskValue and finalMaskValue:
+            filtratedByStartOrEndMaskValue(initialMaskValue, finalMaskValue, mask , filtrated) 
+
+
+    print("Filtrated: ", filtrated)
+    return filtrated
 
 
 
+def filtratedByStartOrEndMaskValue(initialMaskValue: str, finalMaskValue: str, mask: dict, filtrated: dict ):
+    
+
+    for priority in filtrated.keys():
+        for m in mask[priority]:
+            if initialMaskValue and finalMaskValue:
+                if m.startswith(initialMaskValue) and m.endswith(finalMaskValue):
+                    filtrated[priority].append(m)
+            elif initialMaskValue and not finalMaskValue:
+                if m.startswith(initialMaskValue):
+                    filtrated[priority].append(m)
+            elif finalMaskValue:
+                if m.endswith(finalMaskValue):
+                    filtrated[priority].append(m)
+
+    return filtrated
+
+
+
+
+    
 
 def fillInMask(maskPriorityPart:list, cuArray:list, returnPriorityPart:list):
     print("The cuArray : ", cuArray)
@@ -53,6 +127,112 @@ def fillInMask(maskPriorityPart:list, cuArray:list, returnPriorityPart:list):
         if len(toUnmask) > 0 and len(toUnmask) == len(maskAsArray):
             unmasked = arrayToString(toUnmask)
             returnPriorityPart.append(unmasked)
+
+
+
+def getCharPos(charPattern: str) -> dict:
+    charPositions = {}
+    pos = []
+    value = []
+
+    for i, char in enumerate(charPattern):
+        if char != "*":
+            pos.append(i)
+            value.append(char)
+
+    charPositions["pos"] = pos
+    charPositions["value"] = value
+    
+    return charPositions
+
+
+def acceptableCharPattern(cp: str):
+    
+    if cp == "_.":
+        return True
+
+    totalAsteristic = 0
+    cpArray = arrayFromString(cp)
+
+    for char in cpArray:
+        category = unicodedata.category(char)
+
+        if char == "*":
+            totalAsteristic += 1
+
+        if char == "*" or category.startswith("L"):
+            pass
+        else:
+            return False
+
+    if totalAsteristic == 0:
+        return False
+        
+    return True     
+
+
+def verifyCompatibility(ogcsa, pttna):
+  if len(ogcsa) != len(pttna) :
+    return True
+  
+  for i in range(0, len(ogcsa)):
+    s1 = ogcsa[i]
+    s2 = pttna[i]
+
+    if s2 == "*":
+      continue
+
+    if s1 != s2:
+      return False
+
+  
+
+  
+  
+  return True
+
+def findInitialChar(s):
+    initialChar = ''
+    for char in s:
+        if char == '*':
+            break
+        
+        initialChar += char
+
+    return initialChar
+
+def findFinalChar(s):
+    finalChar = ''
+    for char in reversed(s):
+        if char == '*':
+            break
+        
+        finalChar = char + finalChar
+    return finalChar
+
+
+def initialMaskValue(length):
+    if length <= 1 :
+        return None
+    
+    mask = ""
+    for i in range(length):
+        mask = mask + str(i) 
+    return mask
+
+
+
+
+def finalMaskValue(length, initialCharLength):
+    if length <= 1 :
+        return None
+    
+    mask = ""
+    for i in reversed(range(initialCharLength)):
+        if len (mask) == length:
+            break
+        mask = mask + str(i) 
+    return mask
 
 def processTask(charsToUse:str, charPattern:str = "_.", rbf: int = 1):
     print("Processing task...")
